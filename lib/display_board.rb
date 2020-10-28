dcf82a8f7da5e954075e7a782b2290a8a2155fca
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-28 18:46:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,8 +4,3 @@ rows = ["  |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "
   def display_board(board)
     puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
   end
-
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def capture_puts{display_board(board)}
-  puts "#{rows[0]}, #{rows[1]}, #{rows[2]}, #{rows[3]}, #{rows[4]}"
-end
