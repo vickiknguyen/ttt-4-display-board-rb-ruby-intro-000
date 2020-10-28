c272c460933bc6e93621c21bf0800cfa32c3fa70
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-28 19:02:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 rows = ["  |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
 
   def display_board(board)
-    puts "#{board[0]}  | #{board[1]}  | #{board[2]}"
-    puts "#{board[3]}  | #{board[4]}  | #{board[5]}"
-    puts "#{board[6]}  | #{board[7]}  | #{board[8]}"
+    puts "#{board[0]}  | #{board[1]} | #{board[2]}"
+    puts "#{board[3]}  | #{board[4]} | #{board[5]}"
+    puts "#{board[6]}  | #{board[7]} | #{board[8]}"
   end
